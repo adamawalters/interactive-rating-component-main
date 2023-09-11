6bdd3c3446c0c8d286f4a947d33dac71dc7adf61
adamawalters <140983941+adamawalters@users.noreply.github.com>
2023-09-11 22:44:32 +0100

    commit

--- thankyou.js
+++ thankyou.js
@@ -4,7 +4,8 @@ let messageEl = document.getElementById('selected-message');
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-$('#card').css('text-align', 'center');
+$('#card').css('text-align', 'center'); /* or in normal 2023 JS: element.style.setProperty("text-align", "center") */
+$('#card').css('align-items', 'center');
 
 let result = urlParams.get("key1");
 console.log("result is "+ result);
